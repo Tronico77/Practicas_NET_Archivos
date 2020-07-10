41cbac6d3f12b052df75257f0e63ed00b1fb656a
Aurelio Quezada Alvarez <jaquezadaa@gmail.com>
2020-07-09 21:16:58 -0500

    Archivo agregado solo para probar el merge de un fork

---
+++ CEscribirStrings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+namespace ArchivosBytes
+{
+    class CEscribirStrings
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
